0ada8ff49b850fb39d098437e608092d8317443b
thepaj <pavla.jureckova@gypri.cz>
2020-12-05 18:57:03 +0100

    Updates

---
+++ precache-manifest.2fd36425e1197fa65e529db779d31a14.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbc87c196f16f0fbb1610e85c1ab8d7e",
+    "url": "/udacity-myreads-project/index.html"
+  },
+  {
+    "revision": "48ee0e555ba7b4df7921",
+    "url": "/udacity-myreads-project/static/css/main.a01db5fd.chunk.css"
+  },
+  {
+    "revision": "15f78edeabb0996669a4",
+    "url": "/udacity-myreads-project/static/js/2.7fc8788b.chunk.js"
+  },
+  {
+    "revision": "39b2ff43c9f6d0f87d96213c53d39949",
+    "url": "/udacity-myreads-project/static/js/2.7fc8788b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ee0e555ba7b4df7921",
+    "url": "/udacity-myreads-project/static/js/main.b99738e8.chunk.js"
+  },
+  {
+    "revision": "2c93e07a57882787efd9",
+    "url": "/udacity-myreads-project/static/js/runtime-main.ec81a8be.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/udacity-myreads-project/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/udacity-myreads-project/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/udacity-myreads-project/static/media/arrow-drop-down.78c6f53b.svg"
+  }
+]);
